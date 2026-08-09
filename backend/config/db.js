509f7f1275f0494c992433c62812f1cba316a4e0
abirham <abirhammuch526@gmail.com>
2026-08-09 10:59:25 +0300

    database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log(`MongoDB connected successfullyyyyyyy👍👍👍: ${conn.connection.host}`);
+    console.log(`MongoDB connected successfully 👍: ${conn.connection.host}`);
 
     return conn;
   } catch (error) {
