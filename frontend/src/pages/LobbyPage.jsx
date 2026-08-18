c6e9105dbeab90c2843d7e72a1bd8a6c2426d8fc
abirham <abirhammuch526@gmail.com>
2026-08-18 10:45:27 +0300

    test

--- frontend/src/pages/LobbyPage.jsx
+++ frontend/src/pages/LobbyPage.jsx
@@ -3,10 +3,11 @@ import BingoPage from "./bingopage/BingoPage";
 import Footer from "../components/footer/Footer";
 
 const LobbyPage = () => {
-  // Don't show Footer in Telegram WebApp
   const hasTelegram = !!window?.Telegram?.WebApp;
 
-  console.log("🏠 [LobbyPage] Rendering - Telegram:", hasTelegram);
+  console.log("🏠 [LobbyPage] Rendering");
+  console.log("📍 Current URL:", window.location.href);
+  console.log("📱 Telegram:", hasTelegram);
 
   return (
     <div style={{ width: "100%", display: "block" }}>
